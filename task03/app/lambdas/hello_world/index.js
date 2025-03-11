7bf6f770adf16db367fa073b576a6d2da7cf8fc9
Achirshman Deb <achirshman_deb@epam.com>
2025-03-11 18:42:35 +0530

    fix task3

--- task03/app/lambdas/hello_world/index.js
+++ task03/app/lambdas/hello_world/index.js
@@ -1,17 +1,24 @@
 exports.handler = async (event) => {
-    // The Lambda function will handle a GET request and return a response
-    if (event.httpMethod === 'GET') {
+    const path = event.rawPath || event.path;
+    const method = event.requestContext?.http?.method || event.httpMethod;
+ 
+    if (path === "/hello" && method === "GET") {
         return {
             statusCode: 200,
+            headers: { "Content-Type": "application/json" },
             body: JSON.stringify({
-                message: 'Hello from Lambda'
-            })
+                statusCode: 200,
+                message: "Hello from Lambda"
+            }),
         };
     } else {
-        // Handle non-GET methods if needed
         return {
-            statusCode: 405,
-            body: JSON.stringify({ message: 'Method Not Allowed' })
+            statusCode: 400,
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({
+                statusCode: 400,
+                message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`,
+            }),
         };
     }
 };

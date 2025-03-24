749b811fbe849476adcc60f45e094df840a2886b
Achirshman Deb <achirshman_deb@epam.com>
2025-03-24 18:54:22 +0530

    added task09

--- task09/app/lambdas/api_handler/index.js
+++ task09/app/lambdas/api_handler/index.js
@@ -1,40 +1,26 @@
-const weatherClient = require('weather-client');
+const weatherClient = require('/opt/weather-client');
 
 exports.handler = async (event) => {
-  const path = event.rawPath || event.path;
-  const method = event.requestContext?.http?.method || event.httpMethod;
+  if (event.rawPath === "/weather" && event.requestContext?.http?.method === "GET") {
+    const forecast = await weatherClient.getForecast();
 
-  if (path === '/weather' && method === 'GET') {
-    try {
-      const weatherData = await weatherClient.getForecast();
-
-      return {
-        statusCode: 200,
-        body: JSON.stringify(weatherData),
-        headers: {
-          'content-type': 'application/json'
-        },
-        isBase64Encoded: false
-      };
-    } catch (err) {
-      return {
-        statusCode: 500,
-        body: JSON.stringify({ error: `Failed to retrieve weather data: ${err.message}` }),
-        headers: {
-          'content-type': 'application/json'
-        },
-        isBase64Encoded: false
-      };
-    }
+    return {
+      statusCode: 200,
+      body: JSON.stringify(forecast), // 🔥 MAKE SURE THIS IS STRINGIFIED
+      headers: {
+        "content-type": "application/json"
+      },
+      isBase64Encoded: false
+    };
   } else {
     return {
       statusCode: 400,
       body: JSON.stringify({
         statusCode: 400,
-        message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`
+        message: `Bad request syntax or unsupported method. Request path: ${event.rawPath}. HTTP method: ${event.requestContext?.http?.method}`
       }),
       headers: {
-        'content-type': 'application/json'
+        "content-type": "application/json"
       },
       isBase64Encoded: false
     };

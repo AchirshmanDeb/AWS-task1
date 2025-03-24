1ca7dfd73fd4bd1b8f2fa5545bf2774e3e16f3d4
Achirshman Deb <achirshman_deb@epam.com>
2025-03-24 18:34:01 +0530

    added task09

---
+++ task09/app/lambdas/api_handler/index.js
@@ -0,0 +1,44 @@
+// api_handler/index.js
+
+const weatherClient = require('../layers/weather_sdk/weather-client');  // Name must match folder/module in Layer
+
+exports.handler = async (event) => {
+  const path = event.rawPath || event.path;
+  const method = event.requestContext?.http?.method || event.httpMethod;
+
+  if (path === '/weather' && method === 'GET') {
+    try {
+      const weatherData = await weatherClient.getForecast();
+
+      return {
+        statusCode: 200,
+        body: JSON.stringify(weatherData),
+        headers: {
+          'content-type': 'application/json'
+        },
+        isBase64Encoded: false
+      };
+    } catch (err) {
+      return {
+        statusCode: 500,
+        body: JSON.stringify({ error: `Failed to retrieve weather data: ${err.message}` }),
+        headers: {
+          'content-type': 'application/json'
+        },
+        isBase64Encoded: false
+      };
+    }
+  } else {
+    return {
+      statusCode: 400,
+      body: JSON.stringify({
+        statusCode: 400,
+        message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`
+      }),
+      headers: {
+        'content-type': 'application/json'
+      },
+      isBase64Encoded: false
+    };
+  }
+};
